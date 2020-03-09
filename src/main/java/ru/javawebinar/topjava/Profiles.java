d11f47b721b7cf3055519ca11e4da1cc2d2c4cc6
Viktor Cherniak <barsetca@gmail.com>
2020-03-09 23:54:03 +0300

    lesson05 5_8_spring_data_jpa.patch

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -1,14 +1,18 @@
 package ru.javawebinar.topjava;
+
 public class Profiles {
     public static final String
             JDBC = "jdbc",
-            JPA = "jpa";
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+            JPA = "jpa",
+            DATAJPA = "datajpa";
+
+
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
+
     public static final String
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
 
-    public static final String ACTIVE_DB = POSTGRES_DB;
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         try {
